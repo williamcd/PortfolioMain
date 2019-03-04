25c96b1888de62df59a74bab1a053ad271e830ab
williamcd <wcdetrick@me.com>
2019-03-04 03:37:19 -0500

    animations and css done. content next. happy.

---
+++ readme.md
@@ -0,0 +1,3 @@
+image: "https://i.imgur.com/XJyximK.png"
+github: "https://github.com/williamcd/williamcd.github.io"
+desc: "Testing text file mapping/rendering"
--- script.js
+++ script.js
@@ -1,20 +1,56 @@
-$("#about").click(function() {
-    $([document.documentElement, document.body]).animate({
-        scrollTop: $("#About").offset().top
-    }, 1000);
-});
-$("#projects").click(function() {
-    $([document.documentElement, document.body]).animate({
-        scrollTop: $("#Projects").offset().top
-    }, 1000);
-});
-$("#pricing").click(function() {
-    $([document.documentElement, document.body]).animate({
-        scrollTop: $("#Pricing").offset().top
-    }, 1000);
-});
-$("#contact").click(function() {
-    $([document.documentElement, document.body]).animate({
-        scrollTop: $("#Contact").offset().top
-    }, 1000);
-});
+$(window).scroll(function() {
+
+    let documentHeight = $(document).height()
+    let contactHeight = $("#Contact").height()
+    let windowHeight = $(window).scrollTop()
+
+    let total = windowHeight + contactHeight
+
+    let percentage = total / documentHeight * 100
+
+    if (percentage < 30) {
+        console.log("About")
+        $("#about").css("background", "#444444")
+        $("#projects").css("background", "none")
+        $("#pricing").css("background", "none")
+        $("#contact").css("background", "none")
+    } else if (percentage >= 30 && percentage < 55) {
+        console.log("Projects")
+        $("#about").css("background", "none")
+        $("#projects").css("background", "#444444")
+        $("#pricing").css("background", "none")
+        $("#contact").css("background", "none")
+    } else if (percentage >= 55 && percentage < 80) {
+        console.log("Pricing")
+        $("#about").css("background", "none")
+        $("#projects").css("background", "none")
+        $("#pricing").css("background", "#444444")
+        $("#contact").css("background", "none")
+    } else if (percentage >= 80) {
+        console.log("Contact")
+        $("#about").css("background", "none")
+        $("#projects").css("background", "none")
+        $("#pricing").css("background", "none")
+        $("#contact").css("background", "#444444")
+    }
+})
+
+$(".button1").click(function() {
+    if ($(this).html() == "about") {
+        $([document.documentElement, document.body]).animate({
+            scrollTop: $("#About").offset().top
+        }, 1000);
+    } else if ($(this).html() == "projects") {
+        $([document.documentElement, document.body]).animate({
+            scrollTop: $("#Projects").offset().top
+        }, 1000);
+    } else if ($(this).html() == "pricing") {
+        $([document.documentElement, document.body]).animate({
+            scrollTop: $("#Pricing").offset().top
+        }, 1000);
+    } else if ($(this).html() == "contact") {
+        $([document.documentElement, document.body]).animate({
+            scrollTop: $("#Contact").offset().top
+        }, 1000);
+    }
+})
